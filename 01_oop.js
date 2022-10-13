a5da97877933bd0093eb1ebdc08cf9ac2e75d2c4
jorsm <jan.oradini@studenti.unitn.it>
2022-10-13 23:22:26 +0200

    initial commit

---
+++ 01_oop.js
@@ -0,0 +1,74 @@
+class Cliente {
+  static _id = 0;
+
+  constructor(nome, cognome, eta) {
+    //Cliente, con un Id unico nome, cognome ed eta del passeggero.
+    if (typeof Cliente._nextId == "undefined") Cliente._nextId = 0;
+    this.id = Cliente._nextId++;
+    this.nome = nome; // ||""
+    this.cognome = cognome; //|| ""
+    this.eta = eta; //|| 0
+    console.log(
+      `Cliente: ${this.id},\n\t${this.nome} ${this.cognome}, ${this.eta}. `
+    );
+  }
+  aggiungiPrenotazione(volo) {
+    volo.aggiungiPrenotazione(this);
+  }
+  rimuoviPrenotazione(volo) {
+    volo.rimuoviPrenotazione(this);
+  }
+}
+class Volo {
+  static _id = 0;
+
+  constructor(max_passeggeri) {
+    // identificativo univoco due var d'istanza denominate posti e attesa
+    //Ogni volo ha un numero max di passeggeri (passato come parametro al costruttore).
+    if (typeof Volo._nextId == "undefined") Volo._nextId = 0;
+    this.id = Volo._nextId++;
+    this.posti = new Map();
+    this.attesa = [];
+    this.max_passeggeri = max_passeggeri; // > 0 ? max_passeggeri : 1;
+    console.log(`Volo: ${this.id},\n\t${this.max_passeggeri}`);
+  }
+
+  aggiungiPrenotazione(cliente) {
+    //Quando un cliente prenota il volo, lo mettiamo in posti se c'e' ancora disponibilita',
+    //altrimenti lo mettiamo in attesa
+    this.posti.size < this.max_passeggeri
+      ? this.posti.set(cliente.id, cliente)
+      : this.attesa.push(cliente);
+  }
+  rimuoviPrenotazione(cliente) {
+    //Quando un cliente in posti disdice il suo volo,
+    //si libera un posto e prendiamo il primo elemento di attesa e lo trasferiamo su posti.
+    if (this.posti.delete(cliente.id)) {
+      let nuovoPasseggero = this.attesa.shift();
+      if (nuovoPasseggero) this.posti.put(nuovoPasseggero.id, nuovoPasseggero);
+    }
+  }
+}
+
+class GestorePrenotazioni {
+  constructor() {
+    this.clienti = [
+      new Cliente("Franco", "Verdi", 25),
+      new Cliente("Lucia", "Bianchi", 23),
+      new Cliente("Mario", "Rossi", 55),
+      new Cliente("Alessandro", "Rosa", 33),
+      new Cliente("Maria", "Verdi", 70),
+    ];
+    this.voli = [new Volo(150), new Volo(175), new Volo(150)];
+
+    let franco = this.clienti[0];
+    let airbus = this.voli[0];
+
+    airbus.aggiungiPrenotazione(franco);
+    franco.rimuoviPrenotazione(airbus);
+    franco.aggiungiPrenotazione(airbus);
+    airbus.rimuoviPrenotazione(franco);
+  }
+}
+
+new GestorePrenotazioni();
---
+++ 02_minmax.js
@@ -0,0 +1,46 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let inputString = "";
+let currentLine = 0;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+  main();
+});
+function readLine() {
+  return inputString[currentLine++];
+}
+/*
+ * Complete the 'miniMax' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+function miniMax(arr) {
+  //input: five integers
+  //1 <= arr[i] <= 10^9
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  arr.forEach((element) => {
+    if (element < min) min = element;
+    if (element > max) max = element;
+    sum = sum + element;
+  });
+  const maxSum = sum - min;
+  const minSum = sum - max;
+  const result = `${minSum} ${maxSum}`;
+  console.log(result);
+  return result;
+}
+////////////////////////////////////
+function main() {
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+  miniMax(arr);
+}
